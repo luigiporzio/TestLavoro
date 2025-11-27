27cdb51684254bfb46392510a42df80c4da4486c
luigiporzio <luigiporzio13@gmail.com>
2025-11-27 08:26:00 +0000

    CustomerListPage with Filters

--- src/Frontend/src/pages/CustomerListPage.tsx
+++ src/Frontend/src/pages/CustomerListPage.tsx
@@ -1,5 +1,102 @@
-export default function CustomerListPage() {
-    return <>
-        <i>Todo customers</i>
-    </>
+import {
+  Paper,
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+  Typography,
+  TextField,
+  Box,
+  styled,
+  tableCellClasses,
+} from "@mui/material";
+
+import { useState, useEffect } from "react";
+
+interface CustomerListQueryResponse {
+    id: number;
+    name: string;
+    address: string;
+    email: string;
+    phone: string;
+    iban: string;
+    categoryCode?: string;
+    categoryDescription?: string;
 }
+
+
+export default function CustomerListPage() {
+    const [list, setList] = useState<CustomerListQueryResponse[]>([]);
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+
+    useEffect( () => {
+        fetch(`/api/customers/list?name=${name}&email=${email}`)
+        .then((response) => {
+            return response.json();
+        })
+        .then((data) => {
+            setList(data as CustomerListQueryResponse[]);
+        });
+    }, [name, email]);
+
+
+    return (
+    <>
+      <Typography variant="h4" sx={{ textAlign: "center", mt: 4, mb: 4 }}>
+        Customers
+      </Typography>
+
+      <Box sx={{ display: "flex", gap:3, mb: 4}}>
+        <TextField
+        label="Filter by Name"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+        ></TextField>
+        <TextField
+        label="Filter by Email"
+        value={email}
+        onChange={(e) => setEmail(e.target.value)}
+        ></TextField>
+      </Box>
+
+      <TableContainer component={Paper}>
+        <Table sx={{minWidth: 650}} aria-label = "simple table">
+            <TableHead>
+                <TableRow>
+                    <StyledTableHeadCell>Name</StyledTableHeadCell>
+                    <StyledTableHeadCell>Address</StyledTableHeadCell>
+                    <StyledTableHeadCell>Email</StyledTableHeadCell>
+                    <StyledTableHeadCell>Phone</StyledTableHeadCell>
+                    <StyledTableHeadCell>Iban</StyledTableHeadCell>
+                    <StyledTableHeadCell>Code</StyledTableHeadCell>
+                    <StyledTableHeadCell>Description</StyledTableHeadCell>
+                </TableRow>
+            </TableHead>
+            <TableBody>
+                {list.map((row) => (
+                    <TableRow key={row.id}>
+                        <TableCell>{row.name}</TableCell>
+                        <TableCell>{row.address}</TableCell>
+                        <TableCell>{row.email}</TableCell>
+                        <TableCell>{row.phone}</TableCell>
+                        <TableCell>{row.iban}</TableCell>
+                        <TableCell>{row.categoryCode}</TableCell>
+                        <TableCell>{row.categoryDescription}</TableCell>
+                    </TableRow>
+                ))}
+            </TableBody>
+        </Table>
+      </TableContainer>
+    </>
+    );
+}
+
+const StyledTableHeadCell = styled(TableCell)(({ theme }) => ({
+  [`&.${tableCellClasses.head}`]: {
+    backgroundColor: theme.palette.primary.light,
+    color: theme.palette.common.white,
+  },
+}));
